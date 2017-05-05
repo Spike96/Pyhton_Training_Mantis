38998fe453e6ad2734bbfd177bcf859e132c6d25
Spike96 <mtvs72@gmail.com>
2017-05-05 21:16:26 +0300

    Added def random_string

--- model/project.py
+++ model/project.py
@@ -6,4 +6,12 @@ class Project:
         self.description = description
         self.id = id
 
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.name, self.description)
 
+    def __eq__(self, other):
+        return (self.name is None or other.name is None or self.name == other.name) \
+               or (self.description is None or other.description is None or self.description == other.description)
+
+    def key(self):
+        return self.name
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -1,4 +1,24 @@
 
+from model.project import Project
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def test_add_project_mantis(app):
+    old_projects = app.project.get_project_list()
+    project = Project(name=random_string("name", 10), description=random_string("description", 30))
+    app.project.create(project)
+    new_projects = app.project.get_project_list()
+    old_projects.append(project)
+    assert sorted(old_projects, key=Project.key) == sorted(new_projects, key=Project.key)
+
+
+
+
 '''def test_add_project(app):
     old_projects = app.project.get_project_list()
     app.project.create()
@@ -6,12 +26,12 @@
     assert len(old_projects) + 1 == len(new_projects)'''
 
 
-def test_add_project(app):
+'''def test_add_project(app):
     username = "administrator"
     password = "root"
     project_name = "new project"
     app.project.create()
-    assert app.soap.soap_project_list(username, password, project_name)
+    assert app.soap.soap_project_list(username, password, project_name)'''
 
 
 
